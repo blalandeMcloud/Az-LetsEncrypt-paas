48a52d55bb41bb46e753bc740a9d0ff29e0b9c4a
blalandeMcloud <107886772+blalandeMcloud@users.noreply.github.com>
2024-02-23 15:13:58 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,20 +12,20 @@ terraform {
 provider "azurerm" {
   features {}
 
-  #LYON3
-  subscription_id = "eccd9223-bed6-4917-80d9-df26ebcab422"
-  client_id       = "e4fe5f69-5528-44ce-8754-e270f2a363cb"
-  client_secret   = "ODt8Q~EN60rb5_0TZCJ18~_ndqNf.jj99GCGbbim"
-  tenant_id       = "f16e2aeb-1014-43ec-be82-abcdfa6e94b7"
+  #SUB HUB
+  subscription_id = "xxxxx"
+  client_id       = "xxxxx"
+  client_secret   = "xxxxx"
+  tenant_id       = "xxxxx"
 }
 
 provider "azurerm" {
   features {}
 
-  #LYON2
+  #SUB PROD
   alias           = "prod"
-  subscription_id = "b2916dd2-5675-43c3-834b-5feff79458ee"
-  client_id       = "e4fe5f69-5528-44ce-8754-e270f2a363cb"
-  client_secret   = "ODt8Q~EN60rb5_0TZCJ18~_ndqNf.jj99GCGbbim"
-  tenant_id       = "f16e2aeb-1014-43ec-be82-abcdfa6e94b7"
+ subscription_id = "xxxxx"
+  client_id       = "xxxxx"
+  client_secret   = "xxxxx"
+  tenant_id       = "xxxxx"
 }
